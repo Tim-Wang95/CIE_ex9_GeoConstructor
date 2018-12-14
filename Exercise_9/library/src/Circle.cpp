aba9f5cb02242ee2db61e01e79bc13a99241e6f7
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-14 16:57:56 +0100

    Delete Circle.cpp

--- Exercise_9/library/src/Circle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Circle.hpp"
-
-Circle::Circle(double x, double y, double r) :
-	x_(x), y_(y), r_(r) {}
-
-bool Circle::inside(double x, double y) const {
-	return ((x-x_)*(x - x_)+ (y - y_)*(y - y_)) <= r_*r_;
-}
