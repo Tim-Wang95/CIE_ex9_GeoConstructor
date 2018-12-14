3f9dec3bb3d73baafb57d355d4c513324d4c82e6
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-14 16:58:19 +0100

    Delete Union.cpp

--- Exercise_9/library/src/Union.cpp
+++
@@ -1,4 +0,0 @@
-#include "Union.hpp"
-
-
-Union::Union(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2) :	AbsOperation(operand1, operand2) {}Union::~Union() {	delete operand1_; delete operand2_;}bool Union::inside(double x, double y) const {	return (operand1_->inside(x, y) || operand2_->inside(x, y));}
