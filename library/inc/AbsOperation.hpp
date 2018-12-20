04e849c9627075506ac6f7fc46eef693c2dfe5b6
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-20 14:25:54 +0100

    Update AbsOperation.hpp

--- library/inc/AbsOperation.hpp
+++ library/inc/AbsOperation.hpp
@@ -1,18 +1,16 @@
 #ifndef ABSOPERATION_HPP
 #define ABSOPERATION_HPP
 
-
 #include "AbsImplicitGeometry.hpp"
 
-
-	class AbsOperation : public AbsImplicitGeometry {
+class AbsOperation : public AbsImplicitGeometry {
 	public:
 		AbsOperation(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2);
-		virtual ~AbsOperation() {};
+		virtual ~AbsOperation();
 		virtual bool inside(double x, double y) const = 0;
 
 		AbsImplicitGeometry* operand1_;
 		AbsImplicitGeometry* operand2_;
-	};
+};
 
 #endif
