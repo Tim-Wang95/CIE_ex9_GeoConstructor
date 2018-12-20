519ba66665ca51699bbf7ecf947ae7772fb95e33
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-20 14:29:05 +0100

    Update Intersection.cpp

--- library/src/Intersection.cpp
+++ library/src/Intersection.cpp
@@ -1,4 +1,15 @@
 #include "Intersection.hpp"
 
 
-Intersection::Intersection(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2) :	AbsOperation(operand1, operand2) {}Intersection::~Intersection() {	delete operand1_; delete operand2_;}bool Intersection::inside(double x, double y) const {	return (operand1_->inside(x, y) && operand2_->inside(x, y));}
+Intersection::Intersection(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2) :
+	AbsOperation(operand1, operand2) {}
+
+/*
+Intersection::~Intersection() {
+	delete operand1_; delete operand2_;
+}
+*/
+
+bool Intersection::inside(double x, double y) const {
+	return (operand1_->inside(x, y) && operand2_->inside(x, y));
+}
