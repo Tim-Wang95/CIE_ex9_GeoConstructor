eaa99d2fab13a95a2912ec40808e5281c40baf45
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-20 14:28:05 +0100

    Update AbsOperation.cpp

--- library/src/AbsOperation.cpp
+++ library/src/AbsOperation.cpp
@@ -1,5 +1,8 @@
 #include "AbsOperation.hpp"
 
+AbsOperation::AbsOperation(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2) :
+	operand1_(operand1), operand2_(operand2) {}
 
-	AbsOperation::AbsOperation(AbsImplicitGeometry* operand1, AbsImplicitGeometry* operand2) :
-		operand1_(operand1), operand2_(operand2) {}
+AbsOperation::~AbsOperation() {
+	delete operand1_; delete operand2_;
+}
