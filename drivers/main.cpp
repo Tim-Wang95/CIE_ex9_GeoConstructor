e8332b5e24915b7de36de749db27d429f44f2d7d
Tim-Wang95 <45359550+Tim-Wang95@users.noreply.github.com>
2018-12-20 14:24:59 +0100

    Update main.cpp

--- drivers/main.cpp
+++ drivers/main.cpp
@@ -1,13 +1,31 @@
-//#include "AbsImplicitGeometry.hpp"
 #include "Circle.hpp"
 #include "Rectangle.hpp"
+#include "Union.hpp"
+#include "Difference.hpp"
+#include "Intersection.hpp"
+#include "implicitgeometry.hpp"
 
 int main( ) {
+	
+	AbsImplicitGeometry* c = new Circle(10,10,4);
+	AbsImplicitGeometry* r = new Rectangle(6, 6, 14, 14);
+	AbsImplicitGeometry* r2 = new Rectangle(9, 0, 11, 20);
+	AbsImplicitGeometry* c2 = new Circle(10, 10, 3);
 
-	Circle c(3.0, -2.0, 0.6);
-	Rectangle r(3.0, -2.0, 0.6, 10);
+	AbsOperation* s1 = new Intersection(c, r);
+	AbsOperation* s2 = new Union(s1, r2);
+	AbsOperation* s3 = new Difference(s2, c2);
 
-	std::cout << r.inside(3.0, -2.0);
+	//implicitgeometry shape(Intersection(c,r));
+	//Intersection(c, r);
+
+	for (double y = 20; y > 0; y-=0.5) {
+		for (double x = 0; x < 20; x+=0.5) {
+			if (s3->inside(x, y)) { std::cout << "+"; }
+			else { std::cout << " "; }
+		}
+		std::cout << std::endl;
+	}
 
     return 0;
 }
